fdf3c61526d70c59e77bd033db7a577a15247d38
Matt <symplymatt@gmail.com>
2023-10-28 00:32:09 +0100

    changed font to Avenir

--- src/pages/dashboard/DashboardView.vue
+++ src/pages/dashboard/DashboardView.vue
@@ -14,7 +14,7 @@ export default {
   name: 'DashboardView',
   data() {
     return {
-      containerClasses: "grid grid-cols-[20%,80%] h-screen bg-[#F4F6F8] h-full",
+      containerClasses: "grid grid-cols-[20%,80%] h-screen bg-[#F4F6F8] h-full dashboard-container",
       mainContentClasses: "bg-[#F4F6F8] h-full",
     };
   },
@@ -23,3 +23,11 @@ export default {
   }
 }
 </script>
+<style>
+  @import url('https://fonts.cdnfonts.com/css/avenir');
+
+  .dashboard-container{
+    font-family: Avenir;
+  }
+</style>
+                

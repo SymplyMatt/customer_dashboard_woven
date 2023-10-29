2a31fd920203866804a4ae174e525a7f571065a9
Matt <symplymatt@gmail.com>
2023-10-29 22:48:08 +0100

    nav-transaction details

--- src/pages/dashboard/TransactionDetailsView.vue
+++ src/pages/dashboard/TransactionDetailsView.vue
@@ -2,6 +2,25 @@
   <TopMain 
       :title="'Transaction Details'"
   />
+  <div class="flex flex-col w-full p-10 gap-10 overflow-y-scroll main-content-dashboard">
+    <div class="flex flex-col gap-1 justify-start">
+      <div class="text-lightBlack font-12 font-normal">
+        <i class="fa-solid fa-angle-left"></i> Back
+      </div>
+      <div class="font-14 font-medium">
+        Type: Payout
+      </div>
+    </div>
+    
+    <div class="flex flex-row justify-between items-center">
+      <div class="">
+
+      </div>
+      <div class="">
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,4 +33,8 @@ export default {
   },
 }
 </script>
-
+<style>
+  .main-content-dashboard{
+    height: calc(100vh - 80px);
+  }
+</style>

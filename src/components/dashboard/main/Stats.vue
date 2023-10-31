f154ab8e5345db1f5c14438b3ef03909304940a9
Matt <symplymatt@gmail.com>
2023-10-31 12:33:19 +0100

    total box data

--- src/components/dashboard/main/Stats.vue
+++ src/components/dashboard/main/Stats.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="mm:flex mm:justify-between items-center w-full gap-5 grid grid-cols-1fr">
-      <TotalBox :item="totalAmountReceived"/>
-      <TotalBox :item="totalPayouts"/>
-      <Chart :item="totalAmountReceived"/>
+      <TotalBox :item="totalAmountReceived" v-if="totalAmountReceived.length > 0"/>
+      <TotalBox :item="totalPayouts"  v-if="totalPayouts.length > 0"/>
+      <Chart :item="totalAmountReceived" v-if="totalPayouts.length > 0"/>
     </div>
   </template>
   
   <script>
-  import TotalBox from './TotalBox.vue'; 
+  import TotalBox from './TotalBox.vue';    
   import Chart from './Chart.vue'; 
+  import fetchData from '../../../config/fetchData';
+  import { ref, onMounted } from 'vue';
   
   export default {
     name: 'Stats',
@@ -16,38 +18,33 @@
       TotalBox,
       Chart
     },
-    data() {
-      return {
-        totalAmountReceived: [
-          {
-            duration: 'Week',
-            amount: '₦10,002,075.65',
-          },
-          {
-            duration: 'Month',
-            amount: '₦20,002,075.65',
-          },
-          {
-            duration: 'Year',
-            amount: '₦30,002,075.65',
-          },
-        ],
-        totalPayouts: [
-          {
-            duration: 'Week',
-            amount: '₦200,002,075.65',
-          },
-          {
-            duration: 'Month',
-            amount: '₦300,002,075.65',
-          },
-          {
-            duration: 'Year',
-            amount: '₦400,002,075.65',
-          },
-        ],
-      };
-    },
+    setup() {
+    const totalPayouts = ref([]);
+    const totalAmountReceived = ref([]);
+    const loading = ref(true);
+    onMounted(async () => {
+      try {
+        const response = await fetchData('get', '/getTotals');
+        if (response.status === 200) {
+          totalAmountReceived.value = response.data.totalAmountReceived;
+          totalPayouts.value = response.data.totalPayouts;
+          loading.value = false;
+          console.log('totalAmountReceived.value: ', totalAmountReceived.value);
+        } else {
+          loading.value = false; 
+        }
+      } catch (error) {
+        console.error('An error occurred while fetching data');
+        loading.value = false;
+      }
+    });
+
+    return {
+      totalPayouts,
+      totalAmountReceived,
+      loading
+    };
+  },
   }
   </script>
   
